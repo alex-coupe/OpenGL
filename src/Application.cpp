fe52281600612e22acd8551b375ce58479005572
Alex <acooupe@gmail.com>
2021-02-19 15:19:31 +0000

    Drawing two triangles with one vertex buffer

--- src/Application.cpp
+++ src/Application.cpp
@@ -9,15 +9,17 @@
 #include <sstream>
 
 const float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-    0.5f, -0.5, 0.0f,
-    0.5f, 0.5f, 0.0f,
-    -0.5f, 0.5f, 0.0f
+    -0.9f, -0.9f, 0.0f,
+    -0.1f, -0.9f, 0.0f,
+    -0.5f, 0.5f, 0.0f,
+    0.1f, -0.9f, 0.0f,
+    0.9f, -0.9f, 0.0f,
+    0.5f, 0.5f, 0.0f
 };
 
 const unsigned short indices[] = {
     0, 1, 2,
-    2, 3, 0
+    3, 4, 5
 };
 
 #define VERTEX_SHADER 1
