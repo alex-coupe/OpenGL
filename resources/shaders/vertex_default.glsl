5c97ebbc903549a2cd58b7b71a2a261415539982
Alex <acooupe@gmail.com>
2021-03-02 16:18:44 +0000

    lighting calcs in world space

--- resources/shaders/vertex_default.glsl
+++ resources/shaders/vertex_default.glsl
@@ -4,7 +4,9 @@ layout (location = 1) in vec3 aNormal;
 
 out vec3 Normal;
 out vec3 FragPos;
+out vec3 LightPos;
 
+uniform vec3 lightPos;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
@@ -12,6 +14,7 @@ uniform mat4 projection;
 void main()
 {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
-   Normal = mat3(transpose(inverse(model))) * aNormal; 
-   FragPos = vec3(model * vec4(aPos,1.0f));
+   Normal = mat3(transpose(inverse(view * model))) * aNormal; 
+   FragPos = vec3(view * model * vec4(aPos,1.0f));
+   LightPos = vec3(view * model * vec4(lightPos,1.0f));
 }
