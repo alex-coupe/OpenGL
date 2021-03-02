512aec5981bf35aec78ea1d5f7a761878e62fb9a
Alex <acooupe@gmail.com>
2021-03-02 16:28:08 +0000

    Gouraud

--- resources/shaders/frag_default.glsl
+++ resources/shaders/frag_default.glsl
@@ -2,30 +2,13 @@
 
 out vec4 FragColor;
 
-in vec3 FragPos;
-in vec3 Normal;
-in vec3 LightPos;
+in vec3 LightingColor;
 
-uniform vec3 lightColor;
 uniform vec3 objectColor;
+
 
 void main()
 {
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
-
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(LightPos - FragPos); 
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
-
-    float specularStrength = 0.5f;
-    vec3 viewDir = normalize(-FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir,reflectDir),0.0),32);
-
-    vec3 specular = specularStrength * spec * lightColor;
-
-    vec3 result = (ambient + diffuse + specular) * objectColor;
-    FragColor = vec4(result, 1.0f);
+   
+    FragColor = vec4(LightingColor * objectColor, 1.0f);
 } 
