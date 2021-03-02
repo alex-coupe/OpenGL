8c26bcffc92d7610ae088be0feb073ce4ae18c43
Alex <acooupe@gmail.com>
2021-03-02 16:34:23 +0000

    back to normal

--- resources/shaders/vertex_default.glsl
+++ resources/shaders/vertex_default.glsl
@@ -2,10 +2,9 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 
-out vec3 LightingColor;
-
-uniform vec3 viewPos;
-uniform vec3 lightColor;
+out vec3 Normal;
+out vec3 FragPos;
+out vec3 LightPos;
 
 uniform vec3 lightPos;
 uniform mat4 model;
@@ -15,24 +14,7 @@ uniform mat4 projection;
 void main()
 {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
-    vec3 Normal = mat3(transpose(inverse(model))) * aNormal; 
-    vec3 Position = vec3(model * vec4(aPos,1.0f));
-
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
-
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - Position); 
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
-
-    float specularStrength = 1.0f;
-    vec3 viewDir = normalize(viewPos-Position);
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir,reflectDir),0.0),256);
-
-    vec3 specular = specularStrength * spec * lightColor;
-
-    LightingColor = ambient + diffuse + specular;
-   
+   Normal = mat3(transpose(inverse(model))) * aNormal; 
+   FragPos = vec3( model * vec4(aPos,1.0f));
+   LightPos = vec3( model * vec4(lightPos,1.0f));
 }
